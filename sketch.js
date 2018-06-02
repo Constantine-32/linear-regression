eaa84eb6fa56f6aa6adb18a195c0cf2d3b11b4ea
Christian C. Romero <id.32@outlook.com>
2018-06-02 04:55:11 +0200

    added LR Least Squares logic

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 'use strict';
 
+const CWidth = 500
+const CHeigh = 500
+let points = []
+let m = 0
+let b = 0
+
 function setup() {
-  createCenteredCanvas(500, 500)
+  createCenteredCanvas(CWidth, CHeigh)
   background('#005073')
   frameRate(60)
 }
@@ -15,5 +21,39 @@ function createCenteredCanvas(w, h) {
 }
 
 function draw() {
-  // draw loop
+  translate(CWidth / 2, CHeigh / 2)
+  background('#005073')
+  drawPoints()
+  if (points.length > 1)
+    drawLine()
+}
+
+function drawPoints() {
+  stroke('#fff')
+  strokeWeight(3)
+  for (const p of points)
+    point(p.x, -p.y)
+}
+
+function drawLine() {
+  const x1 = -CWidth / 2
+  const y1 = m * x1 + b
+  const x2 = CWidth / 2
+  const y2 = m * x2 + b
+  strokeWeight(1)
+  line(x1, -y1, x2, -y2)
+}
+
+function calcLine() {
+  const mx = points.reduce((t, s) => ({x: t.x + s.x})).x / points.length
+  const my = points.reduce((t, s) => ({y: t.y + s.y})).y / points.length
+  const num = points.map(p => (p.x - mx) * (p.y - my)).reduce((t, s) => t + s)
+  const den = points.map(p => (p.x - mx) ** 2).reduce((t, s) => t + s)
+  m = num / den
+  b = my - m * mx
+}
+
+function mouseClicked() {
+  points.push({x: mouseX - CWidth / 2, y: -(mouseY - CHeigh / 2)})
+  calcLine()
 }
